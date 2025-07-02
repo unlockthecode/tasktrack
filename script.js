dd4637efa7764e28c3b9e25157b35eb72c86efcd
Lock <unlockthecodes@gmail.com>
2025-07-02 18:14:43 +0800

    Modified it a little bit and added some js.

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function saveTasks(){
 function delTasks(){
     localStorage.clear
 }
+
+function addTask(){
+    const title = document.getElementById("title").value;
+    const description = document.getElementById("description").value;
+    const status = document.getElementById("status").value;
+
+    if (!title) return alert("Task needs a valid title!");
+    else if (!description) return alert("Task needs a description");
+    else if (status === "Status") return alert("Select a valid status.")
+
+        
+}
